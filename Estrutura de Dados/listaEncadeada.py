1cddfdc23711f9c5144a412c28c167a4f9291a93
italo <italovianelli59@hotmail.com>
2021-10-31 17:58:19 -0300

    Linked List implementation

--- Estrutura de Dados/listaEncadeada.py
+++ Estrutura de Dados/listaEncadeada.py
@@ -1,2 +1,41 @@
+class Node:
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+    def __repr__(self) -> str:
+        return '%s -> %s' % (self.data, self.next)
+
 class ListaEncadeada:
-    pass
+    def __init__(self, head = None):
+        self.head = head
+
+    def __repr__(self) -> str:
+        return '[' + str(self.head) + ']'
+
+    def append(self, data):
+        if self.head is None:
+            self.head = Node(data)
+            return
+        current = self. head
+        while current.next != None:
+            current = current.next
+        current.next = Node(data)
+
+    def prepend(self, data):
+        newHead = Node(data)
+        newHead.next = self.head
+        self.head = newHead
+
+    def deleteWithValue(self,data):
+        if self.head is None:
+            return
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next is not None:
+            if current.next.data == data:
+                current.next = current.next.next
+                return
+            current = current.next
