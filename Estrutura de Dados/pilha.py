173957b7ea8e145287feb4edcc0987a09f6014c2
julianafalvess <juuuferreiraalves@gmail.com>
2021-10-31 23:23:52 -0300

    add classes de pilha e de fila

--- Estrutura de Dados/pilha.py
+++ Estrutura de Dados/pilha.py
@@ -1,2 +1,10 @@
 class Pilha:
-    pass
+    def __init__(self):
+        self.pilha = []
+
+    def pop(self):
+        self.pilha.pop()
+
+    def push(self, valor):
+        self.pilha.append(valor)
+
